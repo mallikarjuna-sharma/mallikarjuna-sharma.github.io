e337101221f89215c2de88e9d14322fe786b8d60
mallikarjuna sharma <rmalliksharma96@gmail.com>
2025-11-30 16:58:21 +0000

    build files

---
+++ build/precache-manifest.8a24d51f0c5dc9f18b21c0ff58c61eb8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b27dd32e98adbab2ace1042ca96e700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84039ed91d5b042be836",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a0d63dcb2058774779e8",
+    "url": "/static/css/main.e1d5940e.chunk.css"
+  },
+  {
+    "revision": "84039ed91d5b042be836",
+    "url": "/static/js/2.9821d2b9.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/static/js/2.9821d2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d63dcb2058774779e8",
+    "url": "/static/js/main.bf1d6e5a.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "57fc698c6f4f21d12edbeadfae32b88f",
+    "url": "/static/media/Mallikarjuna_sharma_Resume_0925.57fc698c.pdf"
+  },
+  {
+    "revision": "2364ae1a414c4e11b7451c9e0e17c2fd",
+    "url": "/static/media/fea1.2364ae1a.png"
+  },
+  {
+    "revision": "e4372950f127d45988e4b24887480b54",
+    "url": "/static/media/ios.e4372950.png"
+  },
+  {
+    "revision": "d005e82d836e94907462aff7ece15639",
+    "url": "/static/media/mhealth.d005e82d.png"
+  },
+  {
+    "revision": "fba49fb4a206bbb7ef73dea555adb602",
+    "url": "/static/media/rotary.fba49fb4.png"
+  },
+  {
+    "revision": "086cba9fc0f1a03360ca06630b4bd5ec",
+    "url": "/static/media/tictoe.086cba9f.png"
+  }
+]);
